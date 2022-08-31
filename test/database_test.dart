dbb8d70105e9eb5cce6eb06bae0e3d004d49536d
Nick Dawson <37987430+NicholasDawson@users.noreply.github.com>
2022-08-31 00:52:49 -0400

    Made tests for Routes/Tag CRUD.

--- test/database_test.dart
+++ test/database_test.dart
@@ -4,73 +4,45 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:drift/native.dart';
 
 import 'package:redpoint/database/database.dart';
+import 'package:redpoint/database/models/climb_type/climb_type.dart';
+import 'package:redpoint/database/models/grade/grade_system.dart';
+import 'package:redpoint/database/models/route/route_completed_status.dart';
+import 'package:redpoint/database/models/route/route_difficulty.dart';
+import 'package:redpoint/database/models/route/route_status.dart';
 
 void main() {
   late AppDatabase database;
 
-  // Grade
-  String gradeSystemId = "VSCALE";
-  String gradeSystemLabel = "V-Scale";
-  String gradeLabel = "V5";
-  GradeSystemCompanion gradeSystem = GradeSystemCompanion(id: Value(gradeSystemId), label: Value(gradeSystemLabel));
-  GradeCompanion grade = GradeCompanion(systemId: Value(gradeSystemId), grade: Value(gradeLabel));
-
-  // Climb Type
-  int climbTypeId = 1;
-  String climbTypeLabel = "Boulder";
-  ClimbTypeCompanion climbType = ClimbTypeCompanion(id: Value(climbTypeId), label: Value(climbTypeLabel));
-  ClimbTypeToGradeSystemCompanion climbTypeToGradeSystem = ClimbTypeToGradeSystemCompanion(climbTypeId: Value(climbTypeId), gradeSystemId: Value(gradeSystemId));
-
   // Route
   String routeTitle = "Test Route 1";
   DateTime dateTimeNow = DateTime.now();
-  int statusId = 1;
-  String status1Label = "Completed";
-  int completedStatusId = 1;
-  String completedStatus1Label = "Onsight";
-  int difficultyId = 1;
-  String difficulty1Label = "Easy";
+  String grade = "5.8";
   String routeThoughts = "Fun Route!";
 
-  RouteStatusCompanion routeStatus = RouteStatusCompanion(id: Value(statusId), label: Value(status1Label));
-  RouteCompletedStatusCompanion routeCompletedStatus = RouteCompletedStatusCompanion(id: Value(completedStatusId), label: Value(completedStatus1Label));
-  RouteDifficultyCompanion routeDifficulty = RouteDifficultyCompanion(id: Value(difficultyId), label: Value(difficulty1Label));
   RouteCompanion route = RouteCompanion(
       title: Value(routeTitle),
       date: Value(dateTimeNow),
-      climbTypeId: Value(climbTypeId),
-      gradeSystemId: Value(gradeSystemId),
-      grade: Value(gradeLabel),
-      status: Value(statusId),
-      completedStatus: Value(completedStatusId),
-      difficulty: Value(difficultyId),
+      climbTypeId: Value(ClimbTypeEnum.topRope.id),
+      gradeSystemId: Value(GradeSystemEnum.yds.id),
+      grade: Value(grade),
+      status: Value(RouteStatusEnum.completed.id),
+      completedStatus: Value(RouteCompletedStatusEnum.onsight.id),
+      difficulty: Value(RouteDifficultyEnum.intermediate.id),
       thoughts: Value(routeThoughts)
   );
 
-  _initializeData() async {
-    await database.gradeSystemDao.add(gradeSystem);
-    await database.gradeDao.add(grade);
-
-    await database.climbTypeDao.add(climbType);
-    await database.climbTypeToGradeSystemDao.add(climbTypeToGradeSystem);
-
-    await database.routeStatusDao.add(routeStatus);
-    await database.routeCompletedStatusDao.add(routeCompletedStatus);
-    await database.routeDifficultyDao.add(routeDifficulty);
-  }
-
   setUp(() async {
     database = AppDatabase.test(NativeDatabase.memory(logStatements: true));
-    await _initializeData();
   });
 
   tearDown(() async {
     await database.close();
   });
 
-  test('Routes can be created', () async {
-    final id = await database.routeDao.add(route);
-    RouteData? retrievedRoute = await database.routeDao.getById(id);
+  test('Routes CRUD', () async {
+    final insertedRoute = await database.routeDao.insertReturning(route); // CREATE
+
+    RouteData? retrievedRoute = await database.routeDao.getById(insertedRoute.id); // READ
     expect(retrievedRoute?.title, route.title.value);
     expect(retrievedRoute?.date.isSameDay(route.date.value), true);
     expect(retrievedRoute?.climbTypeId, route.climbTypeId.value);
@@ -80,5 +52,30 @@ void main() {
     expect(retrievedRoute?.completedStatus, route.completedStatus.value);
     expect(retrievedRoute?.difficulty, route.difficulty.value);
     expect(retrievedRoute?.thoughts, route.thoughts.value);
+
+    await database.routeDao.updateById(insertedRoute.id, const RouteCompanion(title: Value("Test Route 2"))); // UPDATE
+    retrievedRoute = await database.routeDao.getById(insertedRoute.id);
+    expect(retrievedRoute?.title, "Test Route 2");
+
+    await database.routeDao.deleteById(insertedRoute.id); // DELETE
+    retrievedRoute = await database.routeDao.getById(insertedRoute.id);
+    expect(retrievedRoute, null);
+
+  });
+
+  test('Tag CRUD', () async {
+    TagCompanion tag = const TagCompanion(label: Value("Crimpy"));  // CREATE
+    final insertedTag =  await database.tagDao.insertReturning(tag);
+
+    TagData? retrievedTag = await database.tagDao.getById(insertedTag.id); // READ
+    expect(retrievedTag?.label, "Crimpy");
+
+    await database.tagDao.updateById(insertedTag.id, const TagCompanion(label: Value("Juggy"))); // UPDATE
+    retrievedTag = await database.tagDao.getById(insertedTag.id);
+    expect(retrievedTag?.label, "Juggy");
+
+    await database.tagDao.deleteById(insertedTag.id); // DELETE
+    retrievedTag = await database.tagDao.getById(insertedTag.id);
+    expect(retrievedTag, null);
   });
 }
