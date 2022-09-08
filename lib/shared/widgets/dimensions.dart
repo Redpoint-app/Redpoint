b0e76e15e8c8387c07a4eb0aba86963e561011c8
Devan Kavalchek <devankav7@gmail.com>
2022-09-08 19:37:12 -0400

    Added analysis

---
+++ lib/shared/widgets/dimensions.dart
@@ -0,0 +1,19 @@
+class IconsDimensions {
+  static const double iconSizeSmall = 18.0;
+}
+
+class ContainersDimensions {
+  static const double widthSmall = 24.0;
+  static const double heightSmall = 24.0;
+}
+
+class TextDimensions {
+  static const double subHeader = 18.0;
+}
+
+class PaddingDimensions {
+  static const double lg = 18.0;
+  static const double md = 8.0;
+  static const double sm = 4.0;
+  static const double xs = 2.0;
+}
