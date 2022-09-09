a3f12213e85deb4a62407f0b99d4e5fbaa1207ca
Devan Kavalchek <devankav7@gmail.com>
2022-09-08 21:09:42 -0400

    Fixed relative package

--- lib/home/widgets/add_route_card.dart
+++ lib/home/widgets/add_route_card.dart
@@ -1,8 +1,7 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:redpoint/add_route/add_page.dart';
 import 'package:redpoint/shared/widgets/dimensions.dart';
-
-import '../../add_route/add_page.dart';
 
 class AddRouteCard extends StatefulWidget {
   const AddRouteCard({super.key});
